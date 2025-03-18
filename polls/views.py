c4a6e6974afb1dc50a100cb8810c52721ff2e7d4
maratnugmanov <maratnugmanov@gmail.com>
2025-03-18 10:53:49 +0500

    modified polls.views.py to use .format() instead of %s in some cases for testing purposes.

--- polls/views.py
+++ polls/views.py
@@ -9,7 +9,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    return HttpResponse("You're looking at question {}.".format(question_id))
 
 
 def results(request, question_id):
